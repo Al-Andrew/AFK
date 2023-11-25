5186912284926bf05f5fe76dcba3b034cc8e673c
Andrei ALDEA <aldea.andrei.977@gmail.com>
2023-11-25 20:50:00 +0200

    xmake: add package versioning

--- packages/a/AFK/xmake.lua
+++ packages/a/AFK/xmake.lua
@@ -1,5 +1,10 @@
 package("AFK")
-    set_urls("https://github.com/Al-Andrew/AFK")
+    set_homepage("https://github.com/Al-Andrew/AFK")
+    set_description("Andrew's field kit. C++ utilities library.")
+
+    add_urls("https://github.com/Al-Andrew/AFK/archive/$(version).tar.gz")
+
+    add_versions("v0.0.1", "998d4f115154007fdc08057e9d86750c0235d6b5")
 
     on_install("windows", function(package)
         import("package.tools.xmake").install(package)
